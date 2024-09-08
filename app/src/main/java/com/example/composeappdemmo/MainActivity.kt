b3c1ef7b7491c21c6a51869f1e4e2a9865afc3e1
rajputjeet <103024942+rajputjeet@users.noreply.github.com>
2024-09-08 21:03:59 +0530

    initial commit

---
+++ app/src/main/java/com/example/composeappdemmo/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.composeappdemmo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.composeappdemmo.ui.theme.ComposeAppDemmoTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeAppDemmoTheme {
+
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "pokemon_list_screen"
+                )
+                {
+
+                    composable("pokemon_list_screen") {
+
+                    }
+
+                    composable("pokemon_details_screen/{dominantColor}/{pokemonName}", arguments = listOf(
+                        navArgument("dominantColor"){
+                            type = NavType.IntType
+                        },
+                        navArgument("pokemonName"){
+                            type = NavType.StringType
+                        }
+                    )) {
+
+                        val dominantColor = remember {
+                          val color =  it.arguments?.getInt("dominantColor")
+                            color?.let { Color(it) } ?: Color.White
+                        }
+                       val pokemonName  = remember {
+                         it.arguments?.getString("pokemonName")
+
+                    }
+                    }
+
+                }
+
+
+            }
+        }
+    }
+}
